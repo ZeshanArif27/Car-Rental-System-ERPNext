a835c40b6f190740dd786a5101ce324424a30080
ZeshanArif27 <zeshanarif.official@gmail.com>
2024-07-03 16:46:36 +0000

    Vehicle Registration doctype update

--- car_rental_system/car_rental_system/doctype/vehicle_registration/vehicle_registration.js
+++ car_rental_system/car_rental_system/doctype/vehicle_registration/vehicle_registration.js
@@ -1,8 +1,84 @@
 // Copyright (c) 2024, Zeeshan arif and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Vehicle Registration", {
-// 	refresh(frm) {
+frappe.ui.form.on("Vehicle Registration", {
+	// refresh(frm) {
+       
+    //     //frm.trigger('purchase_value')
+    //     //console.log(frm.doc.purchase_details);
+	// },
+    purchase_value: function (frm) {
+        calculate_investment(frm);
+    }
+    //     frappe.call({
+    //         method: 'car_rental_system.car_rental_system.doctype.vehicle_registration.vehicle_registration.calculate_total_amount',
+    //         args: {
+    //             'purchase_value':frm.doc.purchase_value,
+    //             'car_owner':frm.doc.car_owner,
+    //             'purchase_details':frm.doc.purchase_details,
+    //         },
+    //         callback: function(r){
+    //             if (frm.doc.car_owner == 'Company'){
+    //                 frm.set_value('amount', r.message)
+    //                 console.log(r.message);
+    //             }
+    //             else{
+    //                 console.log(r.message);
+    //                 frm.set_value('purchase_details',[]);
+    //                 refresh_field("purchase_details");
+    //                 $.each(r.message.investment, function(_i, e) {
+    //                     let entry = frm.add_child('purchase_details');
+    //                     entry.investor_id = e.investor_id;
+    //                     entry.inv_percentage = e.get_percent;
+    //                     entry.investment_amount = e.invested_amount;
+    //                 });
+    //                 refresh_field("purchase_details");
+    //             }
+    //             // frm.set_value('purchase_details.',r.message)
+    //         }
+    //     })
+    // }
 
-// 	},
-// });
+});
+var calculate_investment = function(frm){
+        //return console.log(frm);
+        frappe.call({
+            method: 'car_rental_system.car_rental_system.doctype.vehicle_registration.vehicle_registration.calculate_total_amount',
+            args: {
+                'purchase_value':frm.doc.purchase_value,
+                'car_owner':frm.doc.car_owner,
+                'purchase_details':frm.doc.purchase_details,
+            },
+            callback: function(r){
+                if (frm.doc.car_owner == 'Company'){
+                    frm.set_value({'amount': r.message,'percent': 100})
+                    //console.log(r.message);
+                }
+                else{
+                    //console.log(r.message);
+                    frm.set_value('purchase_details',[]);
+                    refresh_field("purchase_details");
+                    $.each(r.message.investment, function(_i, e) {
+                        let entry = frm.add_child('purchase_details');
+                        entry.investor_id = e.investor_id;
+                        entry.inv_percentage = e.get_percent;
+                        entry.investment_amount = e.invested_amount;
+                    });
+                    refresh_field("purchase_details");
+                }
+                // frm.set_value('purchase_details.',r.message)
+            }
+        })
+    
+}
+
+frappe.ui.form.on("Vehicle Purchasing Detail","inv_percentage",function(frm,cdt,cdn){
+    // let investment = locals[cdt][cdn]
+    calculate_investment(frm);
+    // let investment_amount =  
+    // console.log('zeeshan',investment_amount);
+    //investment.investment_amount = investment_amount
+
+    //frm.refresh_feild('purchase_details')
+    
+})
--- car_rental_system/car_rental_system/doctype/vehicle_registration/vehicle_registration.py
+++ car_rental_system/car_rental_system/doctype/vehicle_registration/vehicle_registration.py
@@ -1,9 +1,60 @@
 # Copyright (c) 2024, Zeeshan arif and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe import _,msgprint
+import json
 
 class VehicleRegistration(Document):
 	pass
+
+@frappe.whitelist()
+def calculate_total_amount(
+		purchase_value: float | int,
+		car_owner: str | list,
+		purchase_details
+): 
+	investment = []
+	sum_of_percent = 0
+	if purchase_value <= 0:
+		frappe.msgprint(_("Purchase Value cannot be less than zero."),raise_exception=True)
+	else:
+		if car_owner == 'Company':
+			amount = calculate_amt_based_on_percent(purchase_value,100)
+			return amount
+		elif car_owner == 'Investors':
+			seen = []
+			for investors in  json.loads(purchase_details):
+				if investors.get('investor_id') not in seen :
+					seen.append(investors.get('investor_id'))
+				else:
+					frappe.msgprint(_(f"Investor already exist please remove the duplicate investor. Investor NO: {investors.get('investor_id')}"),raise_exception=True)
+				# frappe.msgprint(_(investors),raise_exception=True)
+				get_percent = investors.get('inv_percentage')
+				invested_amount = calculate_amt_based_on_percent(purchase_value,get_percent)
+
+				investment_details = {
+					'investor_id': investors.get('investor_id'),
+					'get_percent': get_percent,
+					'invested_amount':invested_amount
+				}
+				investment.append(investment_details)
+				sum_of_percent += get_percent
+
+			if sum_of_percent > 100:
+				frappe.msgprint(_("Sum of Investors percentage cannot be greater than zero."),raise_exception=True)
+
+			return {'sum_of_percent':sum_of_percent,'investment':investment}
+		else:
+			return 'HElo world'
+
+
+def calculate_amt_based_on_percent(
+		purchase_value: int | float,
+		percentage: int | float,
+):
+	value = float((purchase_value * percentage)/100)
+
+	return value
+
