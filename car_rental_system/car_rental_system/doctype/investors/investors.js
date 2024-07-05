95c9cb858160f08975325c43dd836efe5d150bc1
ZeshanArif27 <zeshanarif.official@gmail.com>
2024-07-05 15:06:45 +0000

    Vehicle Registration changes based on RTA and HQ RentalSystem

--- car_rental_system/car_rental_system/doctype/investors/investors.js
+++ car_rental_system/car_rental_system/doctype/investors/investors.js
@@ -2,22 +2,68 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Investors", {
-	refresh(frm) {
-        frm.trigger('total_investment');
-	},
-    total_investment: function (frm){
-        
-            frappe.call({
-                method: 'car_rental_system.car_rental_system.doctype.investors.investors.total_investment',
-                args:{
-                    name: frm.doc.name,
-                },
-                callback: function (r){
-                    frm.set_value('total_investment',r.message.investment);
-                }
-            })
-        
-        
+	// refresh(frm) {
+    //     get_investors_details(frm);
+	// },
+    onload(frm){
+        console.log(frm.doc.inv_details.length);
+        if (frm.doc?.inv_details.length > 0 ){
+            validate_vehicle_registration(frm);
+            get_investors_details(frm);
+        }
+        else{
+            get_investors_details(frm);
+        }
     },
-    
 });
+
+var get_investors_details = function(frm){
+    frappe.call({
+        method: 'car_rental_system.car_rental_system.doctype.investors.investors.get_investment',
+        args:{
+            name: frm.doc.name,
+        },
+        callback: function (r){
+            if (r.message.investment >0 ){
+                frm.set_value('total_investment',r.message.investment);
+                refresh_field("inv_details");
+                console.log(r.message.get_investment);
+                $.each(r.message.get_investment, function (_i, e) {
+                    let entry = frm.add_child('inv_details');
+                    entry.asset_no = e.name;
+                    entry.asset_name = e.model_name;
+                    entry.date_of_investment = e.immarticulation_date;
+                    entry.share_percentage = e.inv_percentage;
+                    entry.amt_of_investment = e.investment_amount;
+                });
+                refresh_field("inv_details");
+                
+            }
+            else{
+                frappe.throw({
+                    title: __('Information'),
+                    indicator: 'yellow',
+                    message: __('Investor doesnot yet own a vehicle')
+                });
+            }
+            
+        }
+    });
+}
+
+var validate_vehicle_registration = function (frm) {
+    seen = []
+   // console.log(frm.doc.inv_details);
+    for (item of frm.doc.inv_details) {
+        if (seen.includes(item.asset_no)) {
+            // frappe.throw({ message: __("Investor already exist or investment percentage is zero"), title: __('Validation Error'), indicator: 'red' });
+            console.log('afroz',seen);
+            return false;
+        }
+        else{
+            seen.push(item.asset_no);
+            console.log('zeeshan',seen);
+            get_investors_details(frm);
+        }
+    }
+}
--- car_rental_system/car_rental_system/doctype/investors/investors.py
+++ car_rental_system/car_rental_system/doctype/investors/investors.py
@@ -9,23 +9,46 @@ from frappe import _,msgprint
 class Investors(Document):
 	pass
 
+# @frappe.whitelist()
+# def total_investment(
+# 	name: str | list,
+# ): 	
+# 	values={
+# 		"name":name
+# 	}
+# 	investment = frappe.db.sql("""SELECT SUM(amt_of_inv) AS investment_sum FROM `tabInvestment Details` tdi WHERE tdi.parent = %(name)s""",values = values, as_dict=1)
+
+# 	if investment[0].investment_sum is not None:
+# 		investment = float(investment[0].investment_sum)
+# 	else:
+# 		investment = 0
+
+# 	response_data = {
+# 		'investment': investment
+# 	}
+# 	return response_data
+
+
 @frappe.whitelist()
-def total_investment(
-	name: str | list,
-): 	
+def get_investment(name: str | list ):
+	
 	values={
 		"name":name
 	}
-	investment = frappe.db.sql("""SELECT SUM(amt_of_inv) AS investment_sum FROM `tabInvestment Details` tdi WHERE tdi.parent = %(name)s""",values = values, as_dict=1)
+	total_investment = 0
+	get_investment = frappe.db.sql("""SELECT tvr.name, tvr.model_name, tvr.immarticulation_date, tvpd.inv_percentage, tvpd.investment_amount FROM `tabVehicle Purchasing Detail` tvpd JOIN tabInvestors ti ON ti.name  = tvpd.investor_id JOIN `tabVehicle Registration` tvr ON tvr.name = tvpd.parent WHERE investor_id  = %(name)s""",values = values, as_dict=1)
 
-	if investment[0].investment_sum is not None:
-		investment = float(investment[0].investment_sum)
+	for investment in get_investment:
+		total_investment += investment.investment_amount
+
+	if total_investment is not None:
+			investment = float(total_investment)
 	else:
 		investment = 0
 
-	response_data = {
-		'investment': investment
+	return {
+		'get_investment':get_investment,
+		'investment':investment
 	}
-	return response_data
-
+	
 
